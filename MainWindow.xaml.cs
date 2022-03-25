888ecd91a0d8c45bbc8e6589ef81a41c9e85526f
Zen89 <zenon_olszewski@o2.pl>
2022-03-25 19:41:24 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace Matrix_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal ObservableCollection<Matrix> MatrixList = new ObservableCollection<Matrix>();
+        internal Collection<Matrix> MatrixCollection = new Collection<Matrix>;
+        Matrix testowy = new Matrix("testowy", 4, 4);
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrepareBinding();
+
+            lvMatrix.ItemsSource = MatrixList;
+            double[,] tablica = new double[testowy.MatrixRows,testowy.MatrixCols];
+            Array.Copy(testowy.MatrixBody, tablica, testowy.MatrixBody.Length);
+
+
+        }
+
+        private void PrepareBinding()
+        {
+            //MatrixList = new ObservableCollection<Matrix>();
+            Matrix newMatrix = new Matrix($"Matrix 0", 4, 4);
+            MatrixList.Add(newMatrix);
+            MatrixList.Add(testowy);
+            double[,] tablica = new double[testowy.MatrixRows, testowy.MatrixCols];
+            Array.Copy(testowy.MatrixBody, tablica, testowy.MatrixBody.Length);
+            
+        }
+
+        private void btnAddMatrix_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            if (MatrixList != null)
+            {
+                count = MatrixList.Count();
+            }
+                        
+            int rows = int.Parse(tbRows.Text);
+            int cols = int.Parse(tbCols.Text);
+            Matrix newMatrix = new Matrix($"Matrix {count}", rows, cols);
+            MatrixList.Add(newMatrix);
+            //MessageBox.Show(newMatrix.ToString());
+        }
+
+        private void ListViewItem_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var item = sender as ListViewItem;
+            if (item != null && item.IsSelected)
+            {
+                var temp = item.Content.ToString();
+                MessageBox.Show(MatrixList.);
+            }
+        }
+    }
+}
---
+++ Matrix.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix_Calculator
+{
+    class Matrix
+    {
+        public string MatrixName { get; set; }
+        public int MatrixRows { get; set; }
+        public int MatrixCols { get; set; }
+        public double[,] MatrixBody;
+
+        public Matrix(string name, int rows, int cols)
+        {
+            MatrixName = name;
+            MatrixRows = rows;
+            MatrixCols = cols;
+            MatrixBody = new double[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    MatrixBody[i, j] = i*j;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{MatrixName}");
+            //for (int i = 0; i < MatrixRows; i++)
+            //{
+            //    sb.Append("\r");
+            //    for (int j = 0; j < MatrixCols; j++)
+            //    {
+            //        sb.Append($"{MatrixBody[i, j]} ".PadLeft(4));
+            //    }
+            //}
+            return sb.ToString();
+        }
+    }
+}
